48def5067889e5551b608d6456afef3c1c059d66
Krzysztof Kowalczyk <krzysiek264@gmail.com>
2025-10-02 00:19:01 +0200

    test

--- board/rockchip/odroidgoa/odroidgoa_status.c
+++ board/rockchip/odroidgoa/odroidgoa_status.c
@@ -121,7 +121,7 @@ int odroid_display_status(int logo_mode, int logo_storage, const char *str)
 
 	if (lcd_init()) {
 		printf("odroid lcd init fail!\n");
-		odroid_drop_errorlog("lcd init fail, check dtb file", 29);
+		odroid_drop_errorlog(sprintf("lcd init fail, check dtb file A: %s B: %s", env_get("hwrev_adc"), env_get("hwdet_adc")), 29);
 		odroid_alert_leds();
 		return -1;
 	}
--- cmd/hwrev.c
+++ cmd/hwrev.c
@@ -60,7 +60,11 @@ int do_hwrev(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 		env_set("dtb_kernel", "rk3326-rg351mp-linux.dtb");
 	}
 	printf("adc0 (hw rev) %d\n", hwrev_adc);
-printf("Model = %s\n",env_get("hwrev"));
+	printf("Model = %s\n",env_get("hwrev"));
+
+	env_set("hwrev_adc", sprintf("%d", hwrev_adc))
+	env_set("hwdet_adc", sprintf("%d", hwdet_adc))
+
 	return CMD_RET_SUCCESS;
 }
 
